a72a6815a3cf1647d2a019e1bbb17bc0e6b9c7e5
Johnny Choo <jchoo156@gmail.com>
2016-10-29 13:30:50 -0700

    change database from sql to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161029191303) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "monsters", force: :cascade do |t|
     t.string   "name_one"
     t.string   "name_two"
