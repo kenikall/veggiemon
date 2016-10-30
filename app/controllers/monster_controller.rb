a8aabaf664e20d7db92831365208225b4801792b
Johnny Choo <jchoo156@gmail.com>
2016-10-30 03:32:11 -0700

    display proper monster on show page

--- app/controllers/monster_controller.rb
+++ app/controllers/monster_controller.rb
@@ -1,6 +1,16 @@
 class MonsterController < ApplicationController
   def show
-    @monster_img = Monster.find(1).image_one
+    monster = Monster.find(params[:id])
+    if monster.xp <= 10
+      @monster = monster.name_one
+      @monster_img = monster.image_one
+    elsif monster.xp > 10 && monster.xp < 20
+      @monster = monster.name_two
+      @monster_img = monster.image_two
+    elsif monster.xp > 20 && monster.xp < 30
+      @monster = monster.name_three
+      @monster_img = monster.image_three
+    end
   end
   
   def new
