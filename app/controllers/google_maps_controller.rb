6932cb31247683590f5dee48b0dd55507e1dd3f7
Johnny Choo <jchoo156@gmail.com>
2016-10-30 11:45:33 -0700

    resize google_maps

--- app/controllers/google_maps_controller.rb
+++ app/controllers/google_maps_controller.rb
@@ -1,8 +1,18 @@
 class GoogleMapsController < ApplicationController
   def index
     @location = "San Francisco, CA, United States"
-    @parks_call = Yelp.client.search(@location, { term: 'park', limit: 20 }).businesses
+
+    @parks_call = Yelp.client.search(@location, { term: 'park', limit: 15 }).businesses
+    
     @parks_locations = @parks_call.map { |park| park.location.display_address[0] }
+    # @p_locations = []
+    # @parks_call.each {|location| @p_locations << [location.name, location.image_url]}
     @parks_longlat = @parks_call.map { |park| [park.location.coordinate.longitude, park.location.coordinate.latitude] }
+
+    @farmers_call = Yelp.client.search(@location, { term: 'farmers market', limit: 15 }).businesses
+
+    @farmers_locations = @farmers_call.map { |farmer| farmer.location.display_address[0] }
+
+    @farmers_longlat = @farmers_call.map {|market| [market.location.coordinate.longitude, market.location.coordinate.latitude]}
   end
 end
